1e6a868e278d243e77256c962d9473319364311b
T0psecurity <phillipscheryl615@gmail.com>
2023-09-15 10:33:33 +0200

    feat: Smart wallet

--- other_contract/SmartWallet.sol
+++ other_contract/SmartWallet.sol
@@ -13,46 +13,50 @@ interface IInterchainQueryRouter {
 }
 
 interface TOTP {
-    function validateTOTP(uint8 _encryptedTOTP, uint256 timestamp) external view returns (bool, uint256);
+    function validateTOTP(uint256 _encryptedTOTP, uint256 timestamp) external view returns (bool, uint256);
 }
 
 contract SmartWallet {
 
-    uint32 constant ethereumDomain = 9000;
-    uint256 lastTOTP = 0;
-    address constant totp = 0x09bce27F2394Ae91049636566acFE7DceFb40CE3;
-    address constant iqsRouter = 0x3c91A95Cb8D32933Bffc273Aaa6Fb57473438D6f;
+    uint32 ethereumDomain;
+    uint256 lastTOTP = 1;
+    address totp;
+    address iqsRouter;
     bytes32 messageId;
 
-    // constructor(_iqsRouter) {
-    //     iqsRouter = _iqsRouter;
-    // }
+    function initialize(uint32 _ethereumDomain, address _totp, address _iqsRouter) public {
+        ethereumDomain = _ethereumDomain;
+        totp = _totp;
+        iqsRouter = _iqsRouter;
+    }
 
     function execute() public view returns (bool) {
-        require(lastTOTP + 3600 * 1000 < block.timestamp, "Need OTP");
+        require(lastTOTP + 3600 < block.timestamp, "Need OTP");
         return true;
     }
 
-    function calltimestamp(uint8 _encryptedTOTP) public {
+    function calltimestamp(uint256 secretKey) public {
         TOTP _validateTOTP = TOTP(totp);
 
         // uint32 _label = 32;
         bytes memory _callback = abi.encodePacked(this.receiveOTP.selector);
         // bytes memory _callback = abi.encodePacked(msg.sender);
-
+        uint256 blocktimestamp = block.timestamp;
+        uint256 _encryptedTOTP = blocktimestamp % 100000;
+        _encryptedTOTP = _encryptedTOTP * secretKey;
         messageId = IInterchainQueryRouter(iqsRouter).query(
             ethereumDomain,
             address(_validateTOTP),
-            abi.encodeCall(_validateTOTP.validateTOTP, (_encryptedTOTP, block.timestamp)),
+            abi.encodeCall(_validateTOTP.validateTOTP, (_encryptedTOTP, blocktimestamp)),
             _callback
         );
     }
 
-    function receiveOTP(bool flag, uint256 timestamp) public{
+    function receiveOTP(bool flag) public{
         require(msg.sender == iqsRouter, "only iqsRouter");
         // (bool, timestamp) = message..
         if (flag) {
-            lastTOTP = timestamp;
+            lastTOTP = block.timestamp;
         } else {
             lastTOTP = lastTOTP;
         }
